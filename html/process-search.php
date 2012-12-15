4db96af1c59c11efd1d6903738a84a9cbf7e8ac3
Shane_Wayne <shane@shane-VirtualBox.(none)>
2012-12-15 15:23:19 +0800

    Restyle the resutl

--- html/process-search.php
+++ html/process-search.php
@@ -11,7 +11,7 @@
 			$count++;
 		}
 	}
-echo "<pre>";
+echo "<pre hidden='hidden'>";
 	if(move_uploaded_file($_FILES["pic"]["tmp_name"],"images/"."test.jpg"))
 	{
 		system("cd images;./Engine ".$count." test.jpg");
@@ -19,16 +19,53 @@ echo "<pre>";
 echo "</pre>";
 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+		<title>EzPic</title>
+		<link href="style.css" rel="stylesheet" type="text/css"/>
+		<script type="text/javascript" src="cart.js"></script>
+	</head>
+	
+	<body>	
+		<div id="header">
+			<span>Search Result</span>
+		</div>
 
-<div id="Result">Hello</div>
+	<div id="contain">
+		
+		<div id="left" class="sur">
+			<div id="nav">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="admin.php">Manage</a></li>
+					<li><a href="loginForm.php">Login</a></li>
+					<hr />
+					<li><a href="search.php">Search</a></li>
+					
+				</ul>
+			</div>
+		</div>
+		
+		<div id="main">
 <script type="text/javascript">
+var div = document.getElementById("main");
 var Res = document.getElementsByTagName('pre')[0];
 Res = Res.innerHTML;
 Res = eval('('+Res+')');
 alert(Res.UploadPicture);
 for (x in Res.Compare)
 {
-  document.write(Res.Compare[x].Method+Res.Compare[x].Choice);
+
+  var botton = document.createElement("botton");
+  botton.innerHTML="Hello";
+  
+  div.appendChild(botton);
+  var ul = document.createElement("ul");
+  ul.className = "Shop_List";
+  div.appendChild(ul);
+//  document.write(Res.Compare[x].Method+Res.Compare[x].Choice);
   if(Res.Compare[x].Choice == "Max")
   {
 //    alert("Max");
@@ -41,11 +78,31 @@ for (x in Res.Compare)
   }
   for(y in Res.Compare[x].Result)
   {
-    document.write(Res.Compare[x].Result[y].CompareResult+"<li><img src=images/"+Res.Compare[x].Result[y].PictureName+"></img></li>");
+    var li = document.createElement("li");
+    var img = document.createElement("img");
+    img.src = "images/"+Res.Compare[x].Result[y].PictureName;
+    li.appendChild(img);
+    ul.appendChild(li);
+//    document.write(Res.Compare[x].Result[y].CompareResult+"<li><img src=images/"+Res.Compare[x].Result[y].PictureName+"></img></li>");
   }
 }
 
 </script>
+</ul>
+		</div>
+		
+	</div>
+	
+	<div id="footer">
+		<?php include 'footer.php' ?>
+	</div>
+		<div id="box">
+		show it!;
+		</div>
+	</body>
+
+</html>
+
 
 <?php
 
